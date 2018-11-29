33eca4b4a4a29ade24d836719483f090be8ab366
gmurray22 <gmurray@stanford.edu>
2018-11-29 11:51:00 -0800

    Update comp_algs.py

--- src/comp_algs.py
+++ src/comp_algs.py
@@ -11,15 +11,22 @@ def weight_function(buyer_pos, buyer_d, seller_pos, seller_d):
 
 def simple_test_cases(some_alg, verbose=False):
     A = time.time()
+    total_value = 0
     sim = simulator.Simulator(weight_function)
-    sim.add_node(pos=(0,0), d=3, k=0, buyer=True)
-    sim.add_node(pos=(1,1), d=1, k=0, buyer=False)
-    sim.add_node(pos=(1,1), d=1, k=2, buyer=False)
-    max_steps = 3
+    sim.add_node(pos=(0,0), d=5, k=0, buyer=True)
+    sim.add_node(pos=(1,1), d=2, k=0, buyer=False)
+    sim.add_node(pos=(0,0), d=2, k=3, buyer=False)
+    max_steps = 5
     for t in range(max_steps):
-        print("Step %d" %t)
-        print(some_alg.compute_matching(sim))
+        #print("Step %d" %t)
+        crit_match = some_alg.compute_matching(sim)
+        #print(crit_match)
+        for match in crit_match:
+            assert match[0] in sim.buyer_nodes
+            total_value += sim.G[match[0]][match[1]]['weight']
+            sim.remove_matching(match[0],match[1])
         sim.advance()
+    print(total_value)
     if verbose:
         B = time.time()
         print("  1. First test completed in {} sec.".format(B - A))

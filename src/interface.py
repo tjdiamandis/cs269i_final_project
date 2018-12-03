9d57d2ef37dbfee020472288fdc61d2da1292f2b
djp42 <djp42@stanford.edu>
2018-12-02 23:48:07 -0800

    some good results finally.

--- src/interface.py
+++ src/interface.py
@@ -20,6 +20,8 @@ class Interface:
             algorithm,
             sim,
             p_node=0.2,
+            min_to_add=1,
+            max_to_add=1,
             p_buyer=0.5,
             size=(10,10),
             dep_distr=(3,1), # to make d constant for all nodes, make variance 0 (duh)
@@ -29,15 +31,16 @@ class Interface:
         self.max_weight_alg = algorithm
         self.sim = sim
         self.p_node = p_node
+        self.min_to_add = min_to_add
+        self.max_to_add = max_to_add
         self.p_buyer = p_buyer
         self.size = size
         self.d_mean = dep_distr[0]
         self.d_var = dep_distr[1]
         self.verbose = verbose
-        self.seed = seed
-        if self.seed:
-            np.random.seed(self.seed)
-            if self.verbose: print("Random seed set to: ", self.seed)
+        if seed:
+            np.random.seed(seed)
+            if self.verbose: print("Random seed set to: ", seed)
 
     def run(self, num_steps):
         self.sim.reset()
@@ -45,7 +48,7 @@ class Interface:
         discarded_buyers = []
         discarded_sellers = []
         for i in range(num_steps):
-            node_added = self._maybe_add_node()
+            node_added = self._maybe_add_node(self.min_to_add, self.max_to_add)
             if node_added and self.verbose:
                 print("Added a node at step ", i)
             matchings = self.max_weight_alg.compute_matching(self.sim)
@@ -57,13 +60,19 @@ class Interface:
             discarded_sellers.append((removed_s, i))
         return weights, discarded_buyers, discarded_sellers
 
-    def _maybe_add_node(self):
-        if np.random.uniform(0,1) > self.p_node: return False
-        pos_x = round(np.random.uniform(0, self.size[0]))
-        pos_y = round(np.random.uniform(0, self.size[1]))
-        d = max(round(np.random.normal(self.d_mean, self.d_var)), 1)
-        is_buyer = True if np.random.uniform(0,1) <= self.p_buyer else False
-        self.sim.add_node((pos_x, pos_y), d, is_buyer, k=0)
-        if self.verbose:
-            print("Node: ({},{}), {}, {}".format(pos_x,pos_y,d,is_buyer))
-        return True
+    def _maybe_add_node(self, min_to_add, max_to_add):
+        if np.random.randint(0,1) > self.p_node: return 0
+        if min_to_add == max_to_add:
+            num_to_add = min_to_add
+        else:
+            num_to_add = np.random.randint(min_to_add, max_to_add)
+        if num_to_add == 0: return 0
+        for i in range(num_to_add):
+            pos_x = round(np.random.uniform(0, self.size[0]))
+            pos_y = round(np.random.uniform(0, self.size[1]))
+            d = max(round(np.random.normal(self.d_mean, self.d_var)), 1)
+            is_buyer = True if np.random.uniform(0,1) <= self.p_buyer else False
+            self.sim.add_node((pos_x, pos_y), d, is_buyer, k=0)
+            if self.verbose:
+                print("Node: ({},{}), {}, {}".format(pos_x,pos_y,d,is_buyer))
+        return num_to_add
